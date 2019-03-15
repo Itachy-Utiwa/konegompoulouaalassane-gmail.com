1fb8799f5257bdeb7d6baae37992d96ab815b855
Itachy-Utiwa <47219315+Itachy-Utiwa@users.noreply.github.com>
2019-03-15 13:29:16 +0000

    Add files via upload

---
+++ verification.js
@@ -0,0 +1,19 @@
+
+    //Récupération
+    var formulaire= document.getElementById('formulaire');
+    var nom= document.getElementById('nom');
+    var prenom= document.getElementById('prenom');
+    var adresse= document.getElementById('civ');
+    var ville= document.getElementById('ville');
+    var recevoir= document.getElementById('recevoir');
+    var courriel= document.getElementById('courriel');
+    var envoyer= document.getElementById('envoyer');
+     var nomErreur = document.getElementById('nomErreur');
+    
+
+    //En cours...
+    
+    function formValidity()
+    {
+        
+    }
